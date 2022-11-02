9c42c7af0ca6ed3b4bd9c5573eddc9e6805902cf
Olstertecn11 <otzunund@miumg.edu.gt>
2022-11-02 10:35:30 -0600

    analizing words

--- include/Machine.h
+++ include/Machine.h
@@ -9,6 +9,8 @@ class Machine{
 public:
   int pointer;
   std::vector<Symbol> symbols;
+  std::vector<std::string> reserved_words;
   Machine();
   void loadFile(std::string);
+  void analyzer(std::string);
 };
--- src/Machine.cpp
+++ src/Machine.cpp
@@ -3,6 +3,12 @@
 
 Machine::Machine(){
   this->pointer = 0;
+  this->reserved_words.push_back("Estados");
+  this->reserved_words.push_back("Inicial");
+  this->reserved_words.push_back("Ha");
+  this->reserved_words.push_back("He");
+  this->reserved_words.push_back("Alfabeto");
+  this->reserved_words.push_back("MT");
 }
 
 void Machine::loadFile(std::string filename){
@@ -12,7 +18,9 @@ void Machine::loadFile(std::string filename){
   if(file.is_open()){
     while(!file.eof()){
       getline(file, cadena);
+      /* cadena.erase(remove(cadena.begin(), cadena.end(), ' ')); */
       std::cout << "Cadena: " << cadena << std::endl;
+      this->analyzer(cadena);
     }
     file.close();
   }
@@ -20,3 +28,23 @@ void Machine::loadFile(std::string filename){
     std::cout << "No se pudo abrir \n";
   }
 }
+
+void Machine::analyzer(std::string line){
+  std::string aux = "";
+  for(int i = 0; i < line.length(); i++){
+    if(line[i] == '('){
+      break;
+    }
+    else{
+      aux += line[i];
+    }
+  }
+  std::cout << "Filtered Line: " << aux << std::endl;
+}
+
+
+
+
+
+
+
